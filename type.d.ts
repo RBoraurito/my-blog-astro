53d619b74d29d3015fd6cbc7268af54a6aeb2159
Ricardo Boraure <ricardoboraure350@gmail.com>
2023-09-02 08:53:11 -0400

    ✨ Add projects page

---
+++ type.d.ts
@@ -0,0 +1,10 @@
+interface MetaData extends pageMetaData {
+  author: string;
+  publishDate: string;
+}
+
+interface pageMetaData {
+  metaTitle: string;
+  metaDescription: string;
+  metaImage: string;
+}
