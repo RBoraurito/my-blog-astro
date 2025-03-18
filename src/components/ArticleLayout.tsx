fc133692462f21e139ef9381e5fa3262691c12e0
RBoraure <me@rboraure.dev>
2025-03-18 13:43:48 -0400

    chore: update types

--- src/components/ArticleLayout.tsx
+++ src/components/ArticleLayout.tsx
@@ -2,7 +2,7 @@ import { Container } from '@components/Container'
 import { formatDate } from '../lib/formatDate'
 import { Prose } from '@components/Prose'
 import { ArrowLeftIcon } from '@components/icon/ArrowLeft'
-import type { ReactNode } from 'react'
+import { type ReactNode } from 'react'
 import type { Article } from '../lib/getAllArticles'
 
 export interface ArticleLayoutProps {
