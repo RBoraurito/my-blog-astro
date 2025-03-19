f32d3dab1a29a7e46c6a5503dc9dd605e62ed114
RBoraure <me@rboraure.dev>
2025-03-19 12:59:02 -0400

    chore: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,14 @@ import tailwind from "@astrojs/tailwind";
 import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro"
 import yaml from '@rollup/plugin-yaml';
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [yaml()]
   },
+  output: 'static',
+  adapter: vercel(),
   integrations: [react(), mdx(), tailwind() , markdoc(), keystatic()],
 });
